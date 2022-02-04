d42915e53105004e6bc2a743f13b7cd891e9c6c8
Joshua Reyes <reyes.j15@gmail.com>
2022-02-04 10:56:05 -0500

    first pass

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const app = express();
-const albumGenre = [
+const albums = [
     {id: 1, genre: 'hip-hop'},
     {id: 2, genre: 'pop' },
     {id: 3, genre: 'r&b'},
-]
+];
+//implement route parameters
+app.get('/albums/:id', (req,res) => {
+  //route parameters
+  const album = albums.find(album => album.id === req.params.id*1);
+  const html = `<div><a href='/'>${album.genre}</a></div>`;
+  res.send(html);
+});
 app.get('/', (req,res) => {
-  const html = albumGenre.map(albumGenre => {
-    return `<div><a href=${albumGenre}</a></div>`;
+  const html = albums.map(album => {
+    return `<div><a href='/albums/${album.id}'>${album.genre}</a></div>`;
   }).join('');
   res.send(html);
   //display list of items in root
 
 });
-//implement route parameters
-app.get('/api/genres/:id', (req,res) => {
-    
-}).join('');
+
 const port = process.env.PORT || 3000; //environment variable
 app.listen(port, () => console.log(`Listneing on port ${port}`))
